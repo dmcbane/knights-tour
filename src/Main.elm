36cad174d5ad032c1eec09c0039944d9a7ea9f91
H. Dale McBane <h.dale.mcbane@gmail.com>
2020-08-16 14:52:19 -0400

    Reorganize and cleanup code

--- src/Main.elm
+++ src/Main.elm
@@ -13,59 +13,11 @@ import Time exposing (every)
 import Tuple
 
 
-cell_size : Int -> Int
-cell_size value =
-    value * 25
-
-
-
 type alias Cell =
     ( Int, Int )
 
 type alias BoardSize =
     ( Int, Int )
-
-board_width: BoardSize -> Int
-board_width bs =
-    Tuple.second bs
-
-board_height: BoardSize -> Int
-board_height bs =
-    Tuple.first bs
-
-decrement_size: Int -> Int
-decrement_size n =
-    let
-        minimum_size = 3
-    in
-        if n <= minimum_size then
-            minimum_size
-        else
-            n - 1
-increment_size: Int -> Int
-increment_size n =
-    let
-        maximum_size = 40
-    in
-        if n >= maximum_size then
-            maximum_size
-        else
-            n + 1
-
-increment_pause: Float -> Float
-increment_pause n =
-    n + 1
-
--- decreasing pause time (ms) increases speed
-decrement_pause: Float -> Float
-decrement_pause n =
-    let
-        minimum_pause = 1
-    in
-        if n <= minimum_pause then
-            minimum_pause
-        else
-            n - 1
 
 type alias Model =
     { path : List Cell
@@ -74,27 +26,103 @@ type alias Model =
     , size : BoardSize
     }
 
-
 type Msg
     = Tick Time.Posix
     | SetStart Cell
     | SetSize BoardSize
     | SetPause Float
 
+boardsize_width: BoardSize -> Int
+boardsize_width bs =
+    Tuple.second bs
 
-initialize_board : BoardSize -> List Cell
-initialize_board board_size =
-            List.range 0 (Tuple.first board_size - 1)
+boardsize_height: BoardSize -> Int
+boardsize_height bs =
+    Tuple.first bs
+
+boardsize_dec: Int -> Int
+boardsize_dec n =
+    let
+        minimum_size = 3
+    in
+        if n <= minimum_size then
+            minimum_size
+        else
+            n - 1
+boardsize_inc: Int -> Int
+boardsize_inc n =
+    let
+        maximum_size = 40
+    in
+        if n >= maximum_size then
+            maximum_size
+        else
+            n + 1
+
+pause_inc: Float -> Float
+pause_inc n =
+    n + 10
+
+-- decreasing pause time (ms) increases speed
+pause_dec: Float -> Float
+pause_dec n =
+    let
+        minimum_pause = 0
+    in
+        if n <= minimum_pause then
+            minimum_pause
+        else
+            n - 10
+
+board_init : BoardSize -> List Cell
+board_init board_size =
+            List.range 0 (boardsize_height board_size - 1)
                 |> andThen
                     (\r ->
-                        List.range 0 (Tuple.second board_size - 1)
+                        List.range 0 (boardsize_width board_size - 1)
                             |> andThen
                                 (\c ->
                                     [ ( r, c ) ]
                                 )
                     )
 
+nextMoves : Model -> Cell -> List Cell
+nextMoves model ( stRow, stCol ) =
+    let
+        c =
+            [ 1, 2, -1, -2 ]
 
+        km =
+            c
+                |> andThen
+                    (\cRow ->
+                        c
+                            |> andThen
+                                (\cCol ->
+                                    if abs cRow == abs cCol then
+                                        []
+
+                                    else
+                                        [ ( cRow, cCol ) ]
+                                )
+                    )
+
+        jumps =
+            List.map (\( kmRow, kmCol ) -> ( kmRow + stRow, kmCol + stCol )) km
+    in
+    List.filter (\j -> List.member j model.board && not (List.member j model.path)) jumps
+
+
+bestMove : Model -> Maybe Cell
+bestMove model =
+    case List.head model.path of
+        Just mph ->
+            minimumBy (List.length << nextMoves model) (nextMoves model mph)
+        _ ->
+            Nothing
+
+
+-- Initialize the application - https://guide.elm-lang.org/effects/
 init : () -> ( Model, Cmd Msg )
 init _ =
     let
@@ -104,7 +132,7 @@ init _ =
 
         -- Initial chess board
         initial_board =
-            initialize_board (initial_size, initial_size)
+            board_init (initial_size, initial_size)
 
         initial_path =
             []
@@ -114,6 +142,7 @@ init _ =
     ( Model initial_path initial_board initial_pause (initial_size, initial_size), Cmd.none )
 
 
+-- View the model - https://guide.elm-lang.org/effects/
 view : Model -> H.Html Msg
 view model =
     let
@@ -176,17 +205,18 @@ view model =
         table_cell =
             [ HA.style "display" "table-cell", HA.style "width" "auto", HA.style "padding" "1px 3px" ]
         rows =
-            board_height model.size
+            boardsize_height model.size
 
         cols =
-            board_width model.size
+            boardsize_width model.size
     in
     H.div
         []
         [ H.h1 center [ H.text "Knight's Tour" ]
+        -- controls
         , H.div
             table
-            [ H.div
+            [ H.div -- labels
                 table_row
                 [ H.div
                     table_cell
@@ -203,22 +233,22 @@ view model =
                 ]
             , H.div
                 table_row
-                [ H.div
+                [ H.div -- Increase
                     table_cell
-                    [ H.button [onClick <| SetSize ( increment_size rows, cols )] [ H.text "▲"] ]
+                    [ H.button [onClick <| SetSize ( boardsize_inc rows, cols )] [ H.text "▲"] ]
                 , H.div
                     table_cell
-                    [ H.button [onClick <| SetSize ( rows, increment_size cols )] [ H.text "▲"] ]
+                    [ H.button [onClick <| SetSize ( rows, boardsize_inc cols )] [ H.text "▲"] ]
                 , H.div
                     table_cell
                     [ H.text ""]
                 , H.div
                     table_cell
-                    [ H.button [onClick <| SetPause ( increment_pause model.pause_ms )] [ H.text "▲"] ]
+                    [ H.button [onClick <| SetPause ( pause_inc model.pause_ms )] [ H.text "▲"] ]
                 ]
             , H.div
                 table_row
-                [ H.div
+                [ H.div -- Value
                     table_cell
                     [ H.text <| String.fromInt rows ]
                 , H.div
@@ -233,27 +263,27 @@ view model =
                 ]
             , H.div
                 table_row
-                [ H.div
+                [ H.div -- Decrease
                     table_cell
-                    [ H.button [onClick <| SetSize ( decrement_size rows, cols )] [ H.text "▼"] ]
+                    [ H.button [onClick <| SetSize ( boardsize_dec rows, cols )] [ H.text "▼"] ]
                 , H.div
                     table_cell
-                    [ H.button [onClick <| SetSize ( rows, decrement_size cols )] [ H.text "▼"] ]
+                    [ H.button [onClick <| SetSize ( rows, boardsize_dec cols )] [ H.text "▼"] ]
                 , H.div
                     table_cell
                     [ H.text ""]
                 , H.div
                     table_cell
-                    [ H.button [onClick <| SetPause ( decrement_pause model.pause_ms )] [ H.text "▼"] ]
+                    [ H.button [onClick <| SetPause ( pause_dec model.pause_ms )] [ H.text "▼"] ]
                 ]
             ]
         , H.h2 center [ H.text "(pick a square)" ]
-        , H.div
+        , H.div -- chess board
             center
             [ svg
                 [ version "1.1"
-                , width (String.fromInt (cell_size cols))
-                , height (String.fromInt (cell_size rows))
+                , width (String.fromInt (25 * cols))
+                , height (String.fromInt (25 * rows))
                 , viewBox
                     (join " "
                         [ String.fromInt 0
@@ -268,47 +298,18 @@ view model =
         , H.h3 center [ H.text <| "Unvisited count : " ++ String.fromInt unvisited ]
         ]
 
-
-nextMoves : Model -> Cell -> List Cell
-nextMoves model ( stRow, stCol ) =
-    let
-        c =
-            [ 1, 2, -1, -2 ]
-
-        km =
-            c
-                |> andThen
-                    (\cRow ->
-                        c
-                            |> andThen
-                                (\cCol ->
-                                    if abs cRow == abs cCol then
-                                        []
-
-                                    else
-                                        [ ( cRow, cCol ) ]
-                                )
-                    )
-
-        jumps =
-            List.map (\( kmRow, kmCol ) -> ( kmRow + stRow, kmCol + stCol )) km
-    in
-    List.filter (\j -> List.member j model.board && not (List.member j model.path)) jumps
-
-
-bestMove : Model -> Maybe Cell
-bestMove model =
-    case List.head model.path of
-        Just mph ->
-            minimumBy (List.length << nextMoves model) (nextMoves model mph)
-        _ ->
-            Nothing
-
+-- Update the model - https://guide.elm-lang.org/effects/
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     let
         mo =
             case msg of
+                SetPause pause ->
+                    { model | pause_ms = pause }
+
+                SetSize board_size ->
+                    { model | board = board_init board_size, path = [], size = board_size }
+
                 SetStart start ->
                     { model | path = [ start ] }
 
@@ -324,21 +325,19 @@ update msg model =
 
                                 Just best ->
                                     { model | path = best :: model.path }
-                SetSize board_size ->
-                    { model | board = initialize_board board_size, path = [], size = board_size }
-                SetPause pause ->
-                    { model | pause_ms = pause }
     in
     ( mo, Cmd.none )
 
 
+-- Subscribe to https://guide.elm-lang.org/effects/
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Time.every model.pause_ms Tick
 
+-- Application entry point
 main: Program () Model Msg
 main =
-    element
+    element -- https://package.elm-lang.org/packages/elm/browser/latest/Browser#element
         { init = init
         , view = view
         , update = update
